9a38fce8f462348a8739f006f76442aef780096c
MAYANS MATÍAS <50926612@est.ort.edu.ar>
2024-06-10 16:02:58 -0300

    Created objects

--- Assets/PersonajeScrypt.cs
+++ Assets/PersonajeScrypt.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PersonajeScrypt : MonoBehaviour
 {
+    int puntosPlayer=0;
+    public Text texto;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,17 @@ public class PersonajeScrypt : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Contacto");
+        PickupScript objeto;
+        objeto = other.GetComponent<PickupScript>();
+        if (objeto.esComida)
+        {
+            puntosPlayer += objeto.puntosItem;
+            if (objeto.esComida)
+            {
+                Destroy(other.gameObject);
+            }
+            texto.text = ;
+        }
     }
 
 }
